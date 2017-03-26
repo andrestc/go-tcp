1214d1f034aee4fac00127ec4f546d8371f77f8b
André Carvalho <asantostc@gmail.com>
2017-03-26 14:37:24 -0300

    parses ethernet type and payload

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"net"
 	"os"
@@ -22,19 +23,39 @@ func main() {
 	}
 }
 
+type EtherType [2]byte
+
+var (
+	ARP = EtherType{0x08, 0x06}
+)
+
+func (e EtherType) String() string {
+	switch e {
+	case ARP:
+		return "ARP"
+	}
+	out := make([]byte, 4)
+	hex.Encode(out, e[:])
+	return fmt.Sprintf("%s", string(out))
+}
+
 type EthernetFrame struct {
-	Dmac net.HardwareAddr
-	Smac net.HardwareAddr
+	Dmac      net.HardwareAddr
+	Smac      net.HardwareAddr
+	EtherType EtherType
+	Payload   []byte
 }
 
 func (f *EthernetFrame) String() string {
-	return fmt.Sprintf("src %s dest %s", f.Smac, f.Dmac)
+	return fmt.Sprintf("src %s dest %s type %s payload size %d", f.Smac, f.Dmac, f.EtherType, len(f.Payload))
 }
 
 func parseEthernetFrame(b []byte) {
 	frame := &EthernetFrame{
-		Dmac: net.HardwareAddr(b[:6:6]),
-		Smac: net.HardwareAddr(b[6:12:12]),
+		Dmac:      net.HardwareAddr(b[:6:6]),
+		Smac:      net.HardwareAddr(b[6:12:12]),
+		EtherType: EtherType{b[12], b[13]},
+		Payload:   b[14:],
 	}
-	fmt.Printf("Parsed Ethernet frame: %s\n", frame)
+	fmt.Printf("Parsed Ethernet frame: %s.\n", frame)
 }
